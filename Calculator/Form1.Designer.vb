f48abf85482b3ff16716f19e22e2e021a8bf1ff1
Igor Kravitz <igorkravitz@yahoo.com>
2021-06-08 20:04:21 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,445 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.Button0 = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.ButtonDot = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.Button5 = New System.Windows.Forms.Button()
+		Me.Button6 = New System.Windows.Forms.Button()
+		Me.Button7 = New System.Windows.Forms.Button()
+		Me.Button8 = New System.Windows.Forms.Button()
+		Me.Button9 = New System.Windows.Forms.Button()
+		Me.ButtonAdd = New System.Windows.Forms.Button()
+		Me.ButtonSubtract = New System.Windows.Forms.Button()
+		Me.ButtonMultiply = New System.Windows.Forms.Button()
+		Me.ButtonDivide = New System.Windows.Forms.Button()
+		Me.ButtonEqual = New System.Windows.Forms.Button()
+		Me.ButtonRatio = New System.Windows.Forms.Button()
+		Me.ButtonPercent = New System.Windows.Forms.Button()
+		Me.ButtonShift = New System.Windows.Forms.Button()
+		Me.ButtonClearError = New System.Windows.Forms.Button()
+		Me.ButtonClear = New System.Windows.Forms.Button()
+		Me.ButtonSign = New System.Windows.Forms.Button()
+		Me.ButtonSqrt = New System.Windows.Forms.Button()
+		Me.ButtonMemoryClear = New System.Windows.Forms.Button()
+		Me.ButtonMemoryRead = New System.Windows.Forms.Button()
+		Me.ButtonMemorySave = New System.Windows.Forms.Button()
+		Me.ButtonMemoryAdd = New System.Windows.Forms.Button()
+		Me.ButtonMemorySub = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.LabelMemory = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'Button0
+		'
+		Me.Button0.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button0.Location = New System.Drawing.Point(12, 398)
+		Me.Button0.Name = "Button0"
+		Me.Button0.Size = New System.Drawing.Size(106, 50)
+		Me.Button0.TabIndex = 1
+		Me.Button0.Text = "0"
+		Me.Button0.UseVisualStyleBackColor = True
+		'
+		'Button1
+		'
+		Me.Button1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button1.Location = New System.Drawing.Point(12, 342)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(50, 50)
+		Me.Button1.TabIndex = 2
+		Me.Button1.Text = "1"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Button2
+		'
+		Me.Button2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button2.Location = New System.Drawing.Point(68, 342)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(50, 50)
+		Me.Button2.TabIndex = 3
+		Me.Button2.Text = "2"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button3
+		'
+		Me.Button3.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button3.Location = New System.Drawing.Point(124, 342)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(50, 50)
+		Me.Button3.TabIndex = 4
+		Me.Button3.Text = "3"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'ButtonDot
+		'
+		Me.ButtonDot.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonDot.Location = New System.Drawing.Point(124, 398)
+		Me.ButtonDot.Name = "ButtonDot"
+		Me.ButtonDot.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonDot.TabIndex = 5
+		Me.ButtonDot.Text = ","
+		Me.ButtonDot.UseVisualStyleBackColor = True
+		'
+		'Button4
+		'
+		Me.Button4.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button4.Location = New System.Drawing.Point(12, 286)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(50, 50)
+		Me.Button4.TabIndex = 6
+		Me.Button4.Text = "4"
+		Me.Button4.UseVisualStyleBackColor = True
+		'
+		'Button5
+		'
+		Me.Button5.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button5.Location = New System.Drawing.Point(68, 286)
+		Me.Button5.Name = "Button5"
+		Me.Button5.Size = New System.Drawing.Size(50, 50)
+		Me.Button5.TabIndex = 7
+		Me.Button5.Text = "5"
+		Me.Button5.UseVisualStyleBackColor = True
+		'
+		'Button6
+		'
+		Me.Button6.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button6.Location = New System.Drawing.Point(124, 286)
+		Me.Button6.Name = "Button6"
+		Me.Button6.Size = New System.Drawing.Size(50, 50)
+		Me.Button6.TabIndex = 8
+		Me.Button6.Text = "6"
+		Me.Button6.UseVisualStyleBackColor = True
+		'
+		'Button7
+		'
+		Me.Button7.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button7.Location = New System.Drawing.Point(12, 230)
+		Me.Button7.Name = "Button7"
+		Me.Button7.Size = New System.Drawing.Size(50, 50)
+		Me.Button7.TabIndex = 9
+		Me.Button7.Text = "7"
+		Me.Button7.UseVisualStyleBackColor = True
+		'
+		'Button8
+		'
+		Me.Button8.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button8.Location = New System.Drawing.Point(68, 230)
+		Me.Button8.Name = "Button8"
+		Me.Button8.Size = New System.Drawing.Size(50, 50)
+		Me.Button8.TabIndex = 10
+		Me.Button8.Text = "8"
+		Me.Button8.UseVisualStyleBackColor = True
+		'
+		'Button9
+		'
+		Me.Button9.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.Button9.Location = New System.Drawing.Point(124, 230)
+		Me.Button9.Name = "Button9"
+		Me.Button9.Size = New System.Drawing.Size(50, 50)
+		Me.Button9.TabIndex = 11
+		Me.Button9.Text = "9"
+		Me.Button9.UseVisualStyleBackColor = True
+		'
+		'ButtonAdd
+		'
+		Me.ButtonAdd.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonAdd.Location = New System.Drawing.Point(180, 398)
+		Me.ButtonAdd.Name = "ButtonAdd"
+		Me.ButtonAdd.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonAdd.TabIndex = 12
+		Me.ButtonAdd.Text = "+"
+		Me.ButtonAdd.UseVisualStyleBackColor = True
+		'
+		'ButtonSubtract
+		'
+		Me.ButtonSubtract.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonSubtract.Location = New System.Drawing.Point(180, 342)
+		Me.ButtonSubtract.Name = "ButtonSubtract"
+		Me.ButtonSubtract.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonSubtract.TabIndex = 13
+		Me.ButtonSubtract.Text = "-"
+		Me.ButtonSubtract.UseVisualStyleBackColor = True
+		'
+		'ButtonMultiply
+		'
+		Me.ButtonMultiply.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMultiply.Location = New System.Drawing.Point(180, 286)
+		Me.ButtonMultiply.Name = "ButtonMultiply"
+		Me.ButtonMultiply.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMultiply.TabIndex = 14
+		Me.ButtonMultiply.Text = "*"
+		Me.ButtonMultiply.UseVisualStyleBackColor = True
+		'
+		'ButtonDivide
+		'
+		Me.ButtonDivide.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonDivide.Location = New System.Drawing.Point(180, 230)
+		Me.ButtonDivide.Name = "ButtonDivide"
+		Me.ButtonDivide.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonDivide.TabIndex = 15
+		Me.ButtonDivide.Text = "/"
+		Me.ButtonDivide.UseVisualStyleBackColor = True
+		'
+		'ButtonEqual
+		'
+		Me.ButtonEqual.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonEqual.Location = New System.Drawing.Point(236, 342)
+		Me.ButtonEqual.Name = "ButtonEqual"
+		Me.ButtonEqual.Size = New System.Drawing.Size(50, 106)
+		Me.ButtonEqual.TabIndex = 16
+		Me.ButtonEqual.Text = "="
+		Me.ButtonEqual.UseVisualStyleBackColor = True
+		'
+		'ButtonRatio
+		'
+		Me.ButtonRatio.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonRatio.Location = New System.Drawing.Point(236, 286)
+		Me.ButtonRatio.Name = "ButtonRatio"
+		Me.ButtonRatio.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonRatio.TabIndex = 17
+		Me.ButtonRatio.Text = "1/x"
+		Me.ButtonRatio.UseVisualStyleBackColor = True
+		'
+		'ButtonPercent
+		'
+		Me.ButtonPercent.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonPercent.Location = New System.Drawing.Point(236, 230)
+		Me.ButtonPercent.Name = "ButtonPercent"
+		Me.ButtonPercent.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonPercent.TabIndex = 18
+		Me.ButtonPercent.Text = "%"
+		Me.ButtonPercent.UseVisualStyleBackColor = True
+		'
+		'ButtonShift
+		'
+		Me.ButtonShift.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonShift.Location = New System.Drawing.Point(12, 174)
+		Me.ButtonShift.Name = "ButtonShift"
+		Me.ButtonShift.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonShift.TabIndex = 19
+		Me.ButtonShift.Text = "←"
+		Me.ButtonShift.UseVisualStyleBackColor = True
+		'
+		'ButtonClearError
+		'
+		Me.ButtonClearError.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonClearError.Location = New System.Drawing.Point(68, 174)
+		Me.ButtonClearError.Name = "ButtonClearError"
+		Me.ButtonClearError.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonClearError.TabIndex = 20
+		Me.ButtonClearError.Text = "CE"
+		Me.ButtonClearError.UseVisualStyleBackColor = True
+		'
+		'ButtonClear
+		'
+		Me.ButtonClear.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonClear.Location = New System.Drawing.Point(124, 174)
+		Me.ButtonClear.Name = "ButtonClear"
+		Me.ButtonClear.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonClear.TabIndex = 21
+		Me.ButtonClear.Text = "C"
+		Me.ButtonClear.UseVisualStyleBackColor = True
+		'
+		'ButtonSign
+		'
+		Me.ButtonSign.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonSign.Location = New System.Drawing.Point(180, 174)
+		Me.ButtonSign.Name = "ButtonSign"
+		Me.ButtonSign.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonSign.TabIndex = 22
+		Me.ButtonSign.Text = "±"
+		Me.ButtonSign.UseVisualStyleBackColor = True
+		'
+		'ButtonSqrt
+		'
+		Me.ButtonSqrt.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonSqrt.Location = New System.Drawing.Point(236, 174)
+		Me.ButtonSqrt.Name = "ButtonSqrt"
+		Me.ButtonSqrt.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonSqrt.TabIndex = 23
+		Me.ButtonSqrt.Text = "√"
+		Me.ButtonSqrt.UseVisualStyleBackColor = True
+		'
+		'ButtonMemoryClear
+		'
+		Me.ButtonMemoryClear.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMemoryClear.Location = New System.Drawing.Point(12, 118)
+		Me.ButtonMemoryClear.Name = "ButtonMemoryClear"
+		Me.ButtonMemoryClear.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMemoryClear.TabIndex = 24
+		Me.ButtonMemoryClear.Text = "MC"
+		Me.ButtonMemoryClear.UseVisualStyleBackColor = True
+		'
+		'ButtonMemoryRead
+		'
+		Me.ButtonMemoryRead.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMemoryRead.Location = New System.Drawing.Point(68, 118)
+		Me.ButtonMemoryRead.Name = "ButtonMemoryRead"
+		Me.ButtonMemoryRead.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMemoryRead.TabIndex = 25
+		Me.ButtonMemoryRead.Text = "MR"
+		Me.ButtonMemoryRead.UseVisualStyleBackColor = True
+		'
+		'ButtonMemorySave
+		'
+		Me.ButtonMemorySave.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMemorySave.Location = New System.Drawing.Point(124, 118)
+		Me.ButtonMemorySave.Name = "ButtonMemorySave"
+		Me.ButtonMemorySave.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMemorySave.TabIndex = 26
+		Me.ButtonMemorySave.Text = "MS"
+		Me.ButtonMemorySave.UseVisualStyleBackColor = True
+		'
+		'ButtonMemoryAdd
+		'
+		Me.ButtonMemoryAdd.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMemoryAdd.Location = New System.Drawing.Point(180, 118)
+		Me.ButtonMemoryAdd.Name = "ButtonMemoryAdd"
+		Me.ButtonMemoryAdd.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMemoryAdd.TabIndex = 27
+		Me.ButtonMemoryAdd.Text = "M+"
+		Me.ButtonMemoryAdd.UseVisualStyleBackColor = True
+		'
+		'ButtonMemorySub
+		'
+		Me.ButtonMemorySub.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+		Me.ButtonMemorySub.Location = New System.Drawing.Point(236, 118)
+		Me.ButtonMemorySub.Name = "ButtonMemorySub"
+		Me.ButtonMemorySub.Size = New System.Drawing.Size(50, 50)
+		Me.ButtonMemorySub.TabIndex = 28
+		Me.ButtonMemorySub.Text = "M-"
+		Me.ButtonMemorySub.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.Label1.Location = New System.Drawing.Point(41, 52)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(245, 63)
+		Me.Label1.TabIndex = 29
+		Me.Label1.Text = "0"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'Label2
+		'
+		Me.Label2.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.Label2.Location = New System.Drawing.Point(12, 9)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(274, 33)
+		Me.Label2.TabIndex = 30
+		Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'LabelMemory
+		'
+		Me.LabelMemory.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.LabelMemory.Location = New System.Drawing.Point(11, 54)
+		Me.LabelMemory.Name = "LabelMemory"
+		Me.LabelMemory.Size = New System.Drawing.Size(24, 60)
+		Me.LabelMemory.TabIndex = 31
+		Me.LabelMemory.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(297, 457)
+		Me.Controls.Add(Me.LabelMemory)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.ButtonMemorySub)
+		Me.Controls.Add(Me.ButtonMemoryAdd)
+		Me.Controls.Add(Me.ButtonMemorySave)
+		Me.Controls.Add(Me.ButtonMemoryRead)
+		Me.Controls.Add(Me.ButtonMemoryClear)
+		Me.Controls.Add(Me.ButtonSqrt)
+		Me.Controls.Add(Me.ButtonSign)
+		Me.Controls.Add(Me.ButtonClear)
+		Me.Controls.Add(Me.ButtonClearError)
+		Me.Controls.Add(Me.ButtonShift)
+		Me.Controls.Add(Me.ButtonPercent)
+		Me.Controls.Add(Me.ButtonRatio)
+		Me.Controls.Add(Me.ButtonEqual)
+		Me.Controls.Add(Me.ButtonDivide)
+		Me.Controls.Add(Me.ButtonMultiply)
+		Me.Controls.Add(Me.ButtonSubtract)
+		Me.Controls.Add(Me.ButtonAdd)
+		Me.Controls.Add(Me.Button9)
+		Me.Controls.Add(Me.Button8)
+		Me.Controls.Add(Me.Button7)
+		Me.Controls.Add(Me.Button6)
+		Me.Controls.Add(Me.Button5)
+		Me.Controls.Add(Me.Button4)
+		Me.Controls.Add(Me.ButtonDot)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.Button0)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.KeyPreview = True
+		Me.MaximizeBox = False
+		Me.MaximumSize = New System.Drawing.Size(315, 504)
+		Me.MinimumSize = New System.Drawing.Size(315, 504)
+		Me.Name = "Form1"
+		Me.Text = "Calculator"
+		Me.ResumeLayout(False)
+
+	End Sub
+	Friend WithEvents Button0 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ButtonDot As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ButtonSubtract As Button
+    Friend WithEvents ButtonMultiply As Button
+    Friend WithEvents ButtonDivide As Button
+    Friend WithEvents ButtonEqual As Button
+    Friend WithEvents ButtonRatio As Button
+    Friend WithEvents ButtonPercent As Button
+    Friend WithEvents ButtonShift As Button
+    Friend WithEvents ButtonClearError As Button
+    Friend WithEvents ButtonClear As Button
+    Friend WithEvents ButtonSign As Button
+    Friend WithEvents ButtonRoot As Button
+    Friend WithEvents ButtonMemoryClear As Button
+    Friend WithEvents ButtonMemoryRead As Button
+    Friend WithEvents ButtonMemorySave As Button
+    Friend WithEvents ButtonMemoryAdd As Button
+    Friend WithEvents ButtonMemorySub As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+	Friend WithEvents ButtonSqrt As Button
+	Friend WithEvents LabelMemory As Label
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,324 @@
+﻿Option Explicit On
+'Option Strict On
+Public Class Form1
+
+  Dim calc As Calculation
+  Dim numberOfDigits As Integer
+
+  Protected Overrides Function ProcessCmdKey(ByRef msg As System.Windows.Forms.Message,
+                                             ByVal keyData As System.Windows.Forms.Keys) As Boolean
+
+    If keyData = Keys.Enter Then
+      'Do something here when Enter is pressed
+      Evaluate()
+      Return True 'Stop the form from processing the key any further
+    End If
+    Return MyBase.ProcessCmdKey(msg, keyData)
+
+  End Function
+
+  Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button0.Click,
+    Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click,
+    Button6.Click, Button7.Click, Button8.Click, Button9.Click, ButtonDot.Click,
+    ButtonClear.Click, ButtonClearError.Click,
+    ButtonAdd.Click, ButtonSubtract.Click, ButtonMultiply.Click, ButtonDivide.Click,
+    ButtonSign.Click, ButtonShift.Click, ButtonRoot.Click, ButtonRatio.Click,
+    ButtonPercent.Click, ButtonMemorySub.Click, ButtonMemorySave.Click, ButtonMemoryRead.Click,
+    ButtonMemoryClear.Click, ButtonMemoryAdd.Click, ButtonEqual.Click, ButtonSqrt.Click
+
+    ActionManagement(sender.Name)
+
+  End Sub
+
+  Private Sub Form_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+    ActionManagement(e.KeyCode.ToString())
+  End Sub
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    calc = New Calculation
+    numberOfDigits = 12
+  End Sub
+
+  Sub ActionManagement(source As String)
+
+    Select Case source
+      Case "Button0", "Button1", "Button2", "Button3", "Button4",
+           "Button5", "Button6", "Button7", "Button8", "Button9",
+           "D0", "NumPad0", "D1", "NumPad1", "D2", "NumPad2",
+           "D3", "NumPad3", "D4", "NumPad4", "D5", "NumPad5",
+           "D6", "NumPad6", "D7", "NumPad7", "D8", "NumPad8",
+           "D9", "NumPad9"
+        AddDigit(source)
+      Case "ButtonClear", "ButtonClearError", "Delete"
+        ClearTable()
+      Case "ButtonDot", "Decimal"
+        AddDot()
+      Case "ButtonEqual", "Enter"
+        Evaluate()
+      Case "ButtonAdd", "ButtonSubtract", "ButtonMultiply", "ButtonDivide",
+           "Divide", "Multiply", "Subtract", "Add"
+        SaveOperation(source)
+      Case "ButtonRatio", "ButtonSqrt"
+        SaveOperation(source)
+        EvaluateUnary()
+      Case "ButtonSign", "OemMinus"
+        ChangeSign()
+      Case "ButtonShift", "Back"
+        Shift()
+      Case "ButtonPercent"
+        CalculatePercent()
+      Case "ButtonMemoryAdd", "ButtonMemorySub", "ButtonMemoryClear", "ButtonMemoryRead", "ButtonMemorySave"
+        ActionsInMemory(source)
+      Case Else
+        MsgBox("No action defined for the button or the key")
+    End Select
+
+  End Sub
+
+  Sub ClearTable()
+
+    calc.Clear()
+    Label1.Text = "0"
+    Label2.Text = ""
+
+  End Sub
+
+  Sub Evaluate()
+
+    calc.operand2 = Label1.Text
+    calc.MathOp()
+    Label1.Text = calc.result
+    Label2.Text = ""
+
+  End Sub
+
+  Sub CalculatePercent()
+
+    If calc.operand1 = "" OrElse calc.operand1 = "0" Then
+      Label1.Text = "0"
+    Else
+      Dim res As Decimal = (CDec(calc.operand1) / 100) * CDec(Label1.Text)
+      Label1.Text = $"{res:##########0.##########}"
+    End If
+
+  End Sub
+
+  Sub ActionsInMemory(source As String)
+
+    source = Normalize(source)
+    calc.SetMemory(source, Label1.Text, LabelMemory.Text)
+
+  End Sub
+
+  Sub EvaluateUnary()
+
+    calc.MathOp()
+    Label1.Text = calc.result
+
+  End Sub
+
+  Sub ChangeSign()
+
+    If Label1.Text = "" OrElse Label1.Text = "0" Then
+      Return
+    End If
+    If Label1.Text.IndexOf("-") = -1 Then
+      Label1.Text = "-" & Label1.Text
+    Else
+      Label1.Text = Replace(Label1.Text, "-", "")
+    End If
+
+  End Sub
+
+  Sub SaveOperation(name As String)
+
+    name = Normalize(name)
+    calc.operand1 = Label1.Text
+    calc.SetCurrentop(name)
+    Label2.Text = calc.postfix
+
+  End Sub
+
+  Sub AddDot()
+
+    If Label1.Text.IndexOf(",") > -1 OrElse Label1.Text.Length = numberOfDigits Then
+      Return
+    Else
+      Label1.Text &= ","
+      calc.newNumber = False
+    End If
+
+  End Sub
+
+  Shared Function Normalize(source As String) As String
+
+    source = Replace(source, "Button", "")
+    source = Replace(source, "Memory", "")
+    If source.IndexOf("Divide") = -1 Then
+      source = Replace(source, "D", "")
+    End If
+    source = Replace(source, "NumPad", "")
+    Return source
+
+  End Function
+
+  Sub AddDigit(digit As String)
+
+    digit = Normalize(digit)
+    If Label1.Text.Length = 0 And digit = "0" Then
+      Return
+    End If
+    If Label1.Text = "0" OrElse calc.newNumber Then
+      Label1.Text = digit
+      calc.newNumber = False
+    ElseIf Label1.Text.Length = numberOfDigits Then
+      Return
+    Else
+      Label1.Text &= digit
+    End If
+
+  End Sub
+
+  Sub Shift()
+
+    If Label1.Text.Length <= 1 OrElse Label1.Text = "0" Then
+      Label1.Text = "0"
+      Return
+    End If
+    Label1.Text = Label1.Text.Substring(0, Label1.Text.Length - 1)
+    If Label1.Text.Length > 0 AndAlso Label1.Text.Substring(Label1.Text.Length - 1, 1) = "," Then
+      Label1.Text = Label1.Text.Substring(0, Label1.Text.Length - 1)
+    End If
+
+  End Sub
+
+End Class
+
+Public Class Calculation
+
+  Enum Operation As Byte
+    add
+    subtract
+    multiply
+    divide
+    ratio
+    sqrt
+  End Enum
+
+  Dim currentop As Nullable(Of Operation)
+  Public newNumber As Boolean
+  Public postfix As String
+  Public operand1 As String
+  Public operand2 As String
+  Public result As String
+  Public memory As String
+
+  Sub New()
+
+    Clear()
+    memory = "0"
+
+  End Sub
+
+  Public Sub SetCurrentop(value As String)
+
+    newNumber = True
+    Select Case value
+      Case "Add"
+        currentop = Operation.add
+        postfix = $"{operand1} +"
+      Case "Subtract"
+        currentop = Operation.subtract
+        postfix = $"{operand1} -"
+      Case "Multiply"
+        currentop = Operation.multiply
+        postfix = $"{operand1} *"
+      Case "Divide"
+        currentop = Operation.divide
+        postfix = $"{operand1} /"
+      Case "Ratio"
+        currentop = Operation.ratio
+        postfix = $"reciproc({operand1})"
+      Case "Sqrt"
+        currentop = Operation.sqrt
+        postfix = $"sqrt({operand1})"
+      Case Else
+        MsgBox("Operation not defined")
+        newNumber = False
+    End Select
+
+  End Sub
+
+  Public Sub MathOp()
+
+    Dim res As Decimal
+    Select Case currentop
+      Case Operation.add
+        res = CDec(operand1) + CDec(operand2)
+      Case Operation.subtract
+        res = CDec(operand1) - CDec(operand2)
+      Case Operation.multiply
+        res = CDec(operand1) * CDec(operand2)
+      Case Operation.divide
+        If CDec(operand2) = 0 Then
+          res = 0
+        Else
+          res = CDec(operand1) / CDec(operand2)
+        End If
+      Case Operation.ratio
+        If CDec(operand1) = 0 Then
+          res = 0
+        Else
+          res = 1 / CDec(operand1)
+        End If
+      Case Operation.sqrt
+        res = Math.Sqrt(CDec(operand1))
+    End Select
+    result = $"{res:##########0.##########}"
+    operand1 = result
+    operand2 = "0"
+    currentop = Nothing
+    newNumber = True
+
+  End Sub
+
+  Public Sub Clear()
+
+    operand1 = "0"
+    operand2 = "0"
+    currentop = Nothing
+    newNumber = True
+    postfix = ""
+    result = ""
+
+  End Sub
+
+  Public Sub SetMemory(value As String, ByRef label1 As String, ByRef labelMemory As String)
+
+    Dim res As Decimal
+    newNumber = True
+    Select Case value
+      Case "Add"
+        res = CDec(memory) + CDec(label1)
+        memory = $"{res:##########0.##########}"
+        labelMemory = "M"
+      Case "Sub"
+        res = CDec(memory) - CDec(label1)
+        memory = $"{res:##########0.##########}"
+        labelMemory = "M"
+      Case "Save"
+        memory = label1
+        labelMemory = "M"
+      Case "Read"
+        label1 = memory
+      Case "Clear"
+        memory = "0"
+        labelMemory = ""
+      Case Else
+        MsgBox("Memory operations are undefined")
+        newNumber = False
+    End Select
+
+  End Sub
+
+End Class
